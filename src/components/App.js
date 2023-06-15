bb5765ad47e98d67fe143f457434ad2da17f5282
Shubham Joshi <sjoshi91119@gmail.com>
2023-06-15 08:25:28 +0530

    submitting code-Thu Jun 15 2023 08:25:26 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import TodoList from "./Parent";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <TodoList />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
